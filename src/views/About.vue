011892f78019121a25b2c4554e3544ca3bc203de
zhuxuyong <531132776@qq.com>
2019-07-10 18:46:19 +0800

    新增echarts

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,11 +12,31 @@ require('echarts/lib/chart/bar')
 export default {
   data(){
     return {
-
+      data: [5, 20, 36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,55,99,33,22,11,22,44,
+        60,88,55,99,33,22,11,22,44,31,52,36,60,88,
+        52,36,60,88,55,99,33,22,11,22,44,36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,55,99,60,88,55,99,33,22,11,22,44,31,52,36,60,88,
+        52,36,60,88,55,99,33,22,11,22,44,20, 36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,10, 20,9,55,33,44,22,11,15,61,75,55,99,33,22,11,22,44,36, 10, 10,
+      5, 20, 36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,55,99,33,22,11,22,44,
+        60,88,55,99,33,22,11,22,44,31,52,36,60,88,
+        52,36,60,88,55,99,33,22,11,22,44,36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,52,36,60,88,55,99,33,22,11,22,44,36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,60,88,55,99,60,88,55,99,33,22,11,22,44,31,52,36,60,88,
+        52,36,60,88,55,99,33,22,11,22,44,20, 36, 10, 10, 20,9,55,33,44,22,11,15,61,75,
+      31,52,36,],
+      lists:[]
     }
   },
   mounted(){
-    this.drawLine()
+    this.data.map((v,i) =>{
+      console.log(v,i)
+      this.lists.push(i+1)
+    })
+    console.log(this.lists,'}}}}')
+    this.drawLine();
   },
   methods:{
     drawLine() {
@@ -25,16 +45,99 @@ export default {
       // 绘制图表
       myChart.setOption({
         title: { text: 'ECharts 入门示例' },
-        tooltip: {},
+        // tooltip: {},
+        
         xAxis: {
-          data: [1, 2, 3, 4, 5, 6,7,8,9,10,11]
+          
+          data: this.lists,
+          axisLabel:{
+            formatter: '{value} min'
+          },
+          axisTick:{       //y轴刻度线
+            show:false
+          },
+          lineStyle:{
+            width:0
+          }
         },
-        yAxis: {},
+        yAxis: {
+          axisLine:{
+            show:false,
+          },
+          
+          type:'value',
+          axisLabel:{
+            formatter: '{value} %'
+          },
+          
+          axisTick:{       //y轴刻度线
+            show:false
+          },
+          splitLine:{
+              show:true,
+              lineStyle:{
+                  type:'dashed'
+              }
+          }
+          // data:[0,60,70,80,90,100,120]
+        },
         series: [{
-          name: '销量',
+          // name: '销量',
           type: 'bar',
-          data: [5, 20, 36, 10, 10, 20,9,55,33,44,22,11,15,61,48]
-        }]
+          data: this.data,
+          barMinHeight:0,
+          barMaxHeight:100,
+          itemStyle:{
+            normal:{
+              color:function(v){
+                // console.log(v)
+
+                if(v.value>=0 && v.value<=49){
+                  return '#BDC1C7'
+                }
+                else if(v.value>49 && v.value<=59){
+                  return '#9399A5'
+                }
+                else if(v.value>59 && v.value<=69){
+                  return '#3FA6F2'
+                }
+                else if(v.value>69 && v.value<=79){
+                  return '#14D36B'
+                }
+                else if(v.value>79 && v.value<=89){
+                  return '#FFCB14'
+                }
+                else if(v.value>=90){
+                  return '#F85842'
+                }
+              }
+            }
+          }
+        }],
+        dataZoom: [
+            {
+                type: 'slider',
+                show: false,
+                start: 34,
+                end: 100,
+                handleSize: 8
+            },
+            {
+                type: 'inside',
+                start: 34,
+                end: 100
+            },
+            // {
+            //     type: 'slider',
+            //     show: true,
+            //     yAxisIndex: 0,
+            //     filterMode: 'empty',
+            //     height: '70%',
+            //     handleSize: 8,
+            //     showDataShadow: false,
+            //     left: '93%'
+            // }
+        ],
       });
       window.onresize = function(){
       myChart.resize()
